b2383cc5ce3ced63938b4e285a3094041e6961a9
jcmusical <jcmusical@gmail.com>
2024-05-29 19:05:18 -0500

    Configuracion de plantilla

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
 import express from "express"
+import morgan from 'morgan';
+import {join, dirname} from 'path' //Metodos especificos
+import {fileURLToPath} from 'url'
+import { engine } from 'express-handlebars';
 
-const app = express()
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set("port", process.env.PORT || 4000)
+app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs') 
 
+app.use(morgan('dev')); //Peticiones y estado
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcom"})
+    res.render('index')
 })
+
+app.use(express.static(join(__dirname, 'public')))
 
 export default app;
